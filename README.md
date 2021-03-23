4fc536a6b0d295c9323765b9b3493237e575f5c0
redbeanv <nan_hongbin@naver.com>
2021-03-23 14:35:17 +0900

    add snowpack.config.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # todolist-snowpack
 use snowpack, functional javascript
+
+- snowpack package: https://www.skypack.dev/
---
+++ snowpack.config.js
@@ -0,0 +1,41 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+// https://github.com/snowpackjs/snowpack/blob/main/docs/reference/configuration.md
+
+// snowpack init
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /**
+     * 방식:  [path: string]: string | {url: string, resolve: boolean, static: boolean}
+     * 예:    public: '/' | {url: '/', resolve: true, static: false}
+     *
+     * url: source path와 마운a트 할 url
+     * resolve:
+     * static: true면 빌드 하지 않음
+     * */
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: [
+    /* ... */
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
